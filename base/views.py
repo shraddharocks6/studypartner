1c6c7de15309e97c512aa98eaa72ccd632055287
shraddharocks6 <shraddha6pandey@gmail.com>
2022-02-15 01:04:58 +0530

    Restricting users from editing other user's rooms

--- base/views.py
+++ base/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
@@ -72,9 +73,13 @@ def createRoom(request):
     return render(request, 'base/room_form.html', context)
 
 
+@login_required(login_url='login')
 def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
+
+    if request.user != room.host:
+        return HttpResponse('You are not allowed here !!')
 
     if request.method == 'POST':
         form = RoomForm(request.POST, instance=room)
