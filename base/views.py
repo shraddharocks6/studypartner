9cebff34547ec56ce1919f4d9bff221cb74c6fa7
shraddharocks6 <shraddha6pandey@gmail.com>
2022-02-14 16:12:38 +0530

    First few database migrations

--- base/views.py
+++ base/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse 
+from .models import Room
 
 # Create your views here.
 
 def home(request):
-    return render(request,'home.html')
+    rooms = Room.objects.all()
+    context = {'rooms':rooms}
+    return render(request,'base/home.html',{'rooms':rooms})
 
-def room(request):
-    return render(request,'room.html')
+def room(request,pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room' : room}
+    return render(request,'base/room.html',context)
